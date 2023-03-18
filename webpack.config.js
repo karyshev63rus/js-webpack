e3698a25b37125b8ccddb7a41540e86adab1571b
karyshev63rus <karyshev63rus@gmail.com>
2023-03-18 06:44:35 +0400

    закончена базовая настройка webpack

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,9 @@ module.exports = {
         filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'dist'),
     },
+    devServer: {
+        port: 3000,
+    },
     plugins: [
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, 'src/index.html'),
